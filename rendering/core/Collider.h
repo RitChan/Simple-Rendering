fc5005cf6eae0247ed78b45eb9fd13d1b0ddd270
chenh <chenh0510@qq.com>
2021-08-15 14:30:54 +0800

    update

--- rendering/core/Collider.h
+++ rendering/core/Collider.h
@@ -8,15 +8,15 @@
 
 struct BoundingBox {
    public:
-    int32_t left_ = 0;
-    int32_t right_ = 0;
-    int32_t top_ = 0;
-    int32_t bottom_ = 0;
+    int32_t left_;
+    int32_t right_;
+    int32_t top_;
+    int32_t bottom_; 
 
-    BoundingBox() = default;
+    BoundingBox() : left_(0), right_(0), top_(0), bottom_(0) {}
 
     BoundingBox(int32_t left, int32_t right, int32_t bottom, int32_t top)
-        : left_(left), right_(right), bottom_(bottom), top_(top) {}
+        : left_(left), right_(right), top_(bottom), bottom_(top) {}
 };
 
 #endif  // RENDERING_COLLIDER_H
--- rendering/rasterizer_impl/SimpleRasterizer.cpp
+++ rendering/rasterizer_impl/SimpleRasterizer.cpp
@@ -9,9 +9,9 @@
 void SimpleRasterizer::rasterize(IPrimitive &primitive) {
     auto bbox = primitive.get_bbox();
     for (uint32_t row = std::max(0, bbox.bottom_);
-         row < std::min((int)bbox.top_, (int)height_); row++) {
+         row < (uint32_t)std::min((int)bbox.top_, (int)height_); row++) {
         for (uint32_t col = std::max(0, bbox.left_);
-             col < std::min((int)bbox.right_, (int)width_); col++) {
+             col < (uint32_t)std::min((int)bbox.right_, (int)width_); col++) {
             float cy = row + 0.5f;
             float cx = col + 0.5f;
             primitive.set_cursor(cx, cy);
